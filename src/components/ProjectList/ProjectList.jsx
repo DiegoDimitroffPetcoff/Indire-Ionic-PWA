64e60929a5b744ac02200e8de9d573d7fbbc5600
Diego Dimitroff Petcoff <diegodimitroffpetcoff@hotmail.com>
2024-07-29 14:16:20 +0200

    Fix Css on ProjectList

--- src/components/ProjectList/ProjectList.jsx
+++ src/components/ProjectList/ProjectList.jsx
@@ -1,9 +1,8 @@
 import { IonCol, IonGrid, IonRow, IonContent, IonButton } from "@ionic/react";
 import { useContext } from "react";
-
 import "./ProjectList.css";
-
 import { ProjectContext } from "../../context/ProjectContext";
+
 export function ProjectList() {
   const { projectList, deleteProjectOnList } = useContext(ProjectContext);
 
@@ -12,64 +11,33 @@ export function ProjectList() {
       <IonGrid className="table-grid">
         {/* Caption */}
         <IonRow className="table-caption">
-          <IonCol size="10">Project List</IonCol>
+          <IonCol size="12">Project List</IonCol>
         </IonRow>
 
         {/* Table Header */}
         <IonRow className="table-header">
-          <IonCol size="3" className="table-header-cell">
-            Título
-          </IonCol>
-          <IonCol size="3" className="table-header-cell">
-            Endereço
-          </IonCol>
-
-          <IonCol size="1" className="table-header-cell">
-            Data
-          </IonCol>
-          <IonCol size="3" className="table-header-cell">
-            Relatorio
-          </IonCol>
-          <IonCol size="2" className="table-header-cell"></IonCol>
+          <IonCol sizeMd="3" sizeXs="12" className="table-header-cell">Título</IonCol>
+          <IonCol sizeMd="3" sizeXs="12" className="table-header-cell">Endereço</IonCol>
+          <IonCol sizeMd="1" sizeXs="12" className="table-header-cell">Data</IonCol>
+          <IonCol sizeMd="3" sizeXs="12" className="table-header-cell">Relatorio</IonCol>
+          <IonCol sizeMd="2" sizeXs="12" className="table-header-cell"></IonCol>
         </IonRow>
 
         {projectList.map((project, idProject) => {
           if (project[0].introduction) {
-            const {
-              title,
-              sub_title,
-              main_img_url,
-              address,
-              project_number,
-              date,
-              version,
-            } = project[0].introduction;
+            const { title, address, date, project_number } = project[0].introduction;
 
             return (
               <IonRow key={idProject} className="table-row">
-                <IonCol size="3" className="table-cell">
-                  {title}
-                </IonCol>
-                <IonCol size="3" className="table-cell">
-                  {address}
-                </IonCol>
-                <IonCol size="1" className="table-cell">
-                  {date}
-                </IonCol>
-                <IonCol size="3" className="table-cell">
-                  {date + "_" + title + "_" + project_number}
-                </IonCol>
-                <IonCol size="1" className="table-cell">
+                <IonCol sizeMd="3" sizeXs="12" className="table-cell">{title}</IonCol>
+                <IonCol sizeMd="3" sizeXs="12" className="table-cell">{address}</IonCol>
+                <IonCol sizeMd="1" sizeXs="12" className="table-cell">{date}</IonCol>
+                <IonCol sizeMd="3" sizeXs="12" className="table-cell">{`${date}_${title}_${project_number}`}</IonCol>
+                <IonCol sizeMd="1" sizeXs="12" className="table-cell">
                   <IonButton expand="block">OPEN</IonButton>
                 </IonCol>
-                <IonCol size="1" className="table-cell">
-                  <IonButton
-                    expand="block"
-                    color={"danger"}
-                    onClick={() => deleteProjectOnList(idProject)}
-                  >
-                    X
-                  </IonButton>
+                <IonCol sizeMd="1" sizeXs="12" className="table-cell">
+                  <IonButton expand="block" color={"danger"} onClick={() => deleteProjectOnList(idProject)}>X</IonButton>
                 </IonCol>
               </IonRow>
             );
@@ -78,12 +46,8 @@ export function ProjectList() {
 
         {/* Table Footer */}
         <IonRow className="table-footer">
-          <IonCol size="8" className="table-footer-cell">
-            Total Project
-          </IonCol>
-          <IonCol size="4" className="table-footer-cell">
-            {projectList.length}
-          </IonCol>
+          <IonCol sizeMd="8" sizeXs="6" className="table-footer-cell">Total Project</IonCol>
+          <IonCol sizeMd="4" sizeXs="6" className="table-footer-cell">{projectList.length}</IonCol>
         </IonRow>
       </IonGrid>
     </IonContent>
