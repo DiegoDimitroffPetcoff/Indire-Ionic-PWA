d700a29ee74ffb3d6599d5ce03be17a50b1bcecd
Diego Dimitroff Petcoff <diegodimitroffpetcoff@hotmail.com>
2024-08-15 18:06:53 +0200

    move service worker

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { Msal2Provider } from "@microsoft/mgt-msal2-provider";
 Providers.globalProvider = new Msal2Provider({
   clientId: "d58ed348-f9f6-4d18-96b8-235eec7929c7",
 });
-import * as serviceWorkerRegistration from "../serviceWorkerRegistration.js";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration.js";
 import { PorjectProvider } from "./context/ProjectContext";
 const container = document.getElementById("root");
 const root = createRoot(container!);
