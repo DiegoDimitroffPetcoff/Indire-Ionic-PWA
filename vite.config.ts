21b4a1d4f797e09f6fa5d8d8d92cf92a99cf2bd2
Diego Dimitroff Petcoff <diegodimitroffpetcoff@hotmail.com>
2024-08-25 13:22:45 +0200

    Fix vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,46 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+import { comlink } from "vite-plugin-comlink";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({ registerType: 'autoUpdate' }),
-  ],
-  build: {
-    outDir: 'build',
-    minify: false,
+  plugins: [react(), comlink(), VitePWA({ registerType: "autoUpdate" })],
+  worker: {
+    format: "es", // Cambia a 'es' si usas ES Modules en tu Worker
+    plugins: () => [comlink()],
     rollupOptions: {
-      // Para evitar problemas con Web Workers, asegúrate de que los workers se gestionen correctamente
       output: {
-        // Cambia el formato a 'es' para ser compatible con code-splitting
-        format: 'es', // Asegúrate de que este ajuste esté en el lugar correcto o quítalo si no es necesario
-
-        // Las opciones para los nombres de los archivos
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash].[ext]', // Asegúrate de que los archivos de recursos se gestionen correctamente
+        format: "es",
+        entryFileNames: "assets/[name]-worker.js", // Personaliza el nombre de salida
       },
-      // Asegúrate de que los workers se gestionen correctamente
-      plugins: [
+    },
+  },
+  build: {
+    outDir: "build",
+    minify: "esbuild",
+    rollupOptions: {
+      output: {
+        format: "es",
+
+        chunkFileNames: "assets/[name]-[hash].js",
+        entryFileNames: "assets/[name]-[hash].js",
+        assetFileNames: "assets/[name]-[hash].[ext]",
+      },
+
+      /*       plugins: [
         {
-          name: 'vite-plugin-web-worker',
-          transform (code, id) {
-            if (id.endsWith('?worker')) {
+          name: "vite-plugin-web-worker",
+          transform(code, id) {
+            if (id.endsWith("?worker")) {
               return {
                 code: `export default new Worker(${JSON.stringify(id)})`,
-                map: null
+                map: null,
+                
               };
             }
-          }
-        }
-      ]
+          },
+        },
+      ], */
     },
   },
 });
