e4b14c98eedfc33eeda5724674617123f14b92bd
Diego Dimitroff Petcoff <diegodimitroffpetcoff@hotmail.com>
2024-08-29 10:12:37 +0200

    Fix DateChanger adding validation

--- src/utils/dateChanger.js
+++ src/utils/dateChanger.js
@@ -1,4 +1,5 @@
 export function dateChanger(isoDate) {
+  if (!isoDate) return "";
   const dateObj = new Date(isoDate);
   const day = String(dateObj.getDate()).padStart(2, "0"); // Agregar un 0 inicial si es necesario
   const month = String(dateObj.getMonth() + 1).padStart(2, "0"); // Los meses en JavaScript van de 0 a 11, así que sumamos 1
